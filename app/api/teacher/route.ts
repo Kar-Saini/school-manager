9fd8d98e3ea443e2c8507f0a4d561b3de88e4aa0
Kartik Saini <kackysaini8@gmail.com>
2024-10-09 23:22:47 +0530

    typo

--- app/api/teacher/route.ts
+++ app/api/teacher/route.ts
@@ -1,24 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
-import primsa from "@/app/libs/prismadb";
+import prisma from "@/app/libs/prismadb";
 import bcrypt from "bcrypt";
 
 export async function POST(request: NextRequest) {
   try {
     const body = await request.json();
-    console.log("Hi");
-    const checkIfTeacherExist = await primsa.teacher.findUnique({
+    const checkIfTeacherExist = await prisma.teacher.findUnique({
       where: { email: body.email },
     });
-    console.log(body);
     if (checkIfTeacherExist?.id) {
       return NextResponse.json({ message: "Email ID exists", status: 500 });
     }
     const hasehdPassword = await bcrypt.hash(body.password, 10);
     const teacherDetails = { ...body, password: hasehdPassword };
-    console.log(teacherDetails);
+
     const newTeacher = await prisma.teacher.create({ data: teacherDetails });
-    console.log("created");
-    console.log(newTeacher);
+
     if (newTeacher) {
       return NextResponse.json({
         messgae: "Teacher added",
