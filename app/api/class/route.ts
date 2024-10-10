977681d0cc30a3c0dfa0e07ce225217e60a7f1e5
Kartik Saini <kackysaini8@gmail.com>
2024-10-10 22:54:34 +0530

    class

--- app/api/class/route.ts
+++ app/api/class/route.ts
@@ -2,27 +2,47 @@ import { NextRequest, NextResponse } from "next/server";
 import prisma from "../../libs/prismadb";
 
 export async function POST(request: NextRequest) {
-  const body = await request.json();
-  const classExist = await prisma.class.findFirst({
-    where: { label: body.label },
-  });
-  if (classExist) {
-    return NextResponse.json({
-      message: "Class already exists",
-      classId: classExist.id,
+  try {
+    const body = await request.json();
+    const classExist = await prisma.class.findFirst({
+      where: { label: body.label },
     });
+
+    if (classExist) {
+      return NextResponse.json(
+        { message: "Class already exists", classId: classExist.id },
+        { status: 400 }
+      );
+    }
+
+    const newClass = await prisma.class.create({
+      data: { label: body.label },
+    });
+
+    return NextResponse.json(
+      { message: "Class added", classId: newClass.id },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Error adding class", error: error },
+      { status: 500 }
+    );
   }
-  const newClass = await prisma.class.create({ data: { label: body.label } });
-  return NextResponse.json({ message: "Class added", classId: newClass.id });
 }
 
 export async function GET() {
   try {
     const classesArray = await prisma.class.findMany();
-    if (classesArray) {
-      return NextResponse.json({ message: "Success", classesArray });
-    }
+
+    return NextResponse.json(
+      { message: "Success", classesArray },
+      { status: 200 }
+    );
   } catch (error) {
-    return new Error("Invalide call" + error);
+    return NextResponse.json(
+      { message: "Error fetching classes", error: error },
+      { status: 500 }
+    );
   }
 }
