d5763430fffcc28568d514e0be5b3ffb53a21f99
Kartik Saini <kackysaini8@gmail.com>
2024-10-10 21:51:17 +0530

    prisma

--- global.d.ts
+++ global.d.ts
@@ -1,13 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
-// Extending the global object to include `prisma`
 declare global {
-  namespace NodeJS {
-    interface Global {
-      prisma: PrismaClient | undefined;
-    }
-  }
+  var prisma: PrismaClient | undefined;
 }
 
-// This ensures that the file is treated as a module
 export {};
