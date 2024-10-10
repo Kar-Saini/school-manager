da07cf65ec94cbda5c80d83425dc3643c6059f9f
Kartik Saini <kackysaini8@gmail.com>
2024-10-10 23:02:18 +0530

    chnages

--- app/libs/prismadb.ts
+++ app/libs/prismadb.ts
@@ -1,9 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
-// Ensure `globalThis.prisma` is typed and initialized properly
 const client = globalThis.prisma || new PrismaClient();
 
-// In development, attach the client to globalThis to reuse it across module reloads
 if (process.env.NODE_ENV !== "production") {
   globalThis.prisma = client;
 }
